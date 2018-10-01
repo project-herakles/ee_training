fed5bfccca87c4c441ef9feaac8fa820d64320f1
ThermiteX <thermitex@outlook.com>
2018-10-01 21:33:27 +0800

    Code optimization & Notations

--- ee_assignment_1/ee_assignment_1.ino
+++ ee_assignment_1/ee_assignment_1.ino
@@ -1,6 +1,6 @@
 #include <Servo.h>
-#define LEFT_DEFAULT {9, 2, 7, false, 0, gate_left}
-#define RIGHT_DEFAULT {10, 3, 8, false, 0, gate_right}
+#define LEFT_DEFAULT {9, 2, 7, 0, gate_left}
+#define RIGHT_DEFAULT {10, 3, 8, 0, gate_right}
 
 //The reference position for the servo
 struct Gate_Ref{
@@ -9,27 +9,25 @@ struct Gate_Ref{
 };
 const Gate_Ref gate_left = {130, 50}, gate_right = {90, 170};
 
-enum State {VACANT, OCCUPIED};
-
 struct Device {
   int pin_servo; //the motor controlling the gate.
   int pin_switch;
   int pin_indicator; //one LED, turned on when the gate is opened, turned off otherwise
-  bool occupied;
-  unsigned long starter;
+  unsigned long starter; //record the millis() when hardware interrupt is activated
   Gate_Ref ref;
   Servo servo;
 };
 Device right = RIGHT_DEFAULT;
 Device left = LEFT_DEFAULT;
-unsigned long reset = 86400;
+
+unsigned long reset = 100000; //a big enough number so that currentmillis+2000 cannot reach
 
 void open_left(){
   delay(15);
-  if (digitalRead(left.pin_switch) == LOW){
+  if (digitalRead(left.pin_switch) == LOW){ //double check to prevent glitch
     digitalWrite(left.pin_indicator, HIGH);
     left.servo.write(left.ref.OPEN);
-    left.starter = millis();
+    left.starter = millis(); //record starter
   }
 }
 
@@ -51,7 +49,7 @@ void setup()
   digitalWrite(right.pin_indicator, LOW);
   right.servo.attach(right.pin_servo);
   right.servo.write(right.ref.CLOSE);
-  right.starter = reset;
+  right.starter = reset; //set the default starter
   delay(15);
   
 
@@ -65,30 +63,32 @@ void setup()
   left.starter = reset;
   delay(15);
 
+  //Insert hardware interrupt
   attachInterrupt(digitalPinToInterrupt(left.pin_switch),open_left,FALLING);
   attachInterrupt(digitalPinToInterrupt(right.pin_switch),open_right,FALLING);
-  
+
+  //Insert timer interrupt
   TCCR0A = 0;
   TCNT0 = 0;
   OCR0A = 255;
-  TCCR0A |= (1<<WGM01); // enable the CTC mode
+  TCCR0A |= (1<<WGM01);
   TIMSK0 |= (1<<OCIE0A);
 }
 
 ISR(TIMER0_COMPA_vect)
 {
-  unsigned long currentMillis = millis();
-  if (currentMillis >= right.starter + 2000)
+  unsigned long currentMillis = millis(); //fetch current millis()
+  if (currentMillis >= right.starter + 2000) //if it has already been 2 seconds
   {
-    right.servo.write(right.ref.CLOSE);
     digitalWrite(right.pin_indicator, LOW);
-    right.starter = reset;
+    right.servo.write(right.ref.CLOSE);
+    right.starter = reset; //reset starter
   }
-  currentMillis = millis();
+  currentMillis = millis(); //refetch currentmillis in case it changes
   if (currentMillis >= left.starter + 2000)
   {
-    left.servo.write(left.ref.CLOSE);
     digitalWrite(left.pin_indicator, LOW);
+    left.servo.write(left.ref.CLOSE);
     left.starter = reset;
   }
 }
